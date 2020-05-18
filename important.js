b7f0d48f4c721f4aeb98cc1adf72b7e90688af9c
IRFANSHAHMARWAT <55048890+IRFANSHAHMARWAT@users.noreply.github.com>
2020-05-18 16:40:56 +0500

    Add files via upload

---
+++ important.js
@@ -0,0 +1,10 @@
+<script> window['onload'] = function() {
+    var _0xc4edx3 = document['getElementById']('gdcontent');
+    if (_0xc4edx3 == null) {
+        window['location']['href'] = 'http://www.Marwat-tech.tk'
+    };
+    _0xc4edx3['setAttribute']('href', 'http://www.Marwat-tech.tk/');
+    _0xc4edx3['setAttribute']('rel', 'nofollow');
+    _0xc4edx3['innerHTML'] = 'Distributed by <a href=\'http://www.Marwat-tech.tk\' rel=\'nofollow\' target=\'_blank\'>Marwat-tech</a>';
+};
+     </script> 
